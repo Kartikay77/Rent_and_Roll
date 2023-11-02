407551375b58633001476ab3aab3b87cfac52d4a
unknown <pranathi.borra@gmail.com>
2023-11-02 17:07:10 -0400

    Added CarOwner Object file

---
+++ RentAndRoll/src/Backend/CarOwner.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import java.io.Serializable;
+
+public class CarOwner implements Serializable{
+    private int ownerId;
+    private String  ownerName;
+    private String contactNo;
+    private float balance;
+
+    public CarOwner(int ownerId, String ownerName, String contactNo, float balance) {
+        this.ownerId = ownerId;
+        this.ownerName = ownerName;
+        this.contactNo = contactNo;
+        this.balance = balance;
+    }
+
+    /**
+     * @return the ownerId
+     */
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    /**
+     * @param ownerId the ownerId to set
+     */
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    /**
+     * @return the ownerName
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    /**
+     * @param ownerName the ownerName to set
+     */
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    /**
+     * @return the contactNo
+     */
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    /**
+     * @param contactNo the contactNo to set
+     */
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    /**
+     * @return the balance
+     */
+    public float getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+}
